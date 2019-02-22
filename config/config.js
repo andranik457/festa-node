d27676109c6b5c7f080efee38f59eb1eef2540e8
Andranik <andranikasl@gmail.com>
2019-02-22 15:43:36 +0400

    some changes

--- config/config.js
+++ config/config.js
@@ -7,46 +7,10 @@ const os = require("os");
 
 const config = {
     
-    // mode: "local",
-    // mode: "production",
     mode: "testV101",
 
-    local: {
-        httpUrl: "local-festa.smartsoft.am:8080",
-
-        port: parseInt(process.env.PORT) || 3015,
-
-        hostname : "127.0.0.1",
-
-        mongoConf: {
-            dbName: "festa",
-            url: "mongodb://localhost/festa",
-            options: {
-                server: {
-                    auto_reconnect : true,
-                    reconnectTries : 17280,
-                    reconnectInterval : 5000
-                }
-            }
-        },
-
-        mongoConfFiles: {
-            dbName: "files",
-            url: "mongodb://localhost/files",
-            options: {
-                server: {
-                    auto_reconnect : true,
-                    reconnectTries : 17280,
-                    reconnectInterval : 5000
-                }
-            }
-        },
-
-        jwtSecret: "resti!$ret*&key", // JWT secret
-    },
-
     testV101: {
-        httpUrl: "blahblah.com:8080",
+        httpUrl: "travel.smartsoft.am:8086",
 
         port: parseInt(process.env.PORT) || 3065,
 
@@ -77,52 +41,10 @@ const config = {
         },
 
         jwtSecret: "resti!$ret*&key", // JWT secret
-    },
-
-    production: {
-        httpUrl: "festa.smartsoft.am:8080",
-
-        port: parseInt(process.env.PORT) || 3015,
-
-        hostname : "127.0.0.1",
-
-        mongoConf: {
-            dbName: "festa",
-            url: "mongodb://localhost/festa",
-            options: {
-                server: {
-                    auto_reconnect : true,
-                    reconnectTries : 17280,
-                    reconnectInterval : 5000
-                }
-            }
-        },
-
-        mongoConfFiles: {
-            dbName: "files",
-            url: "mongodb://localhost/files",
-            options: {
-                server: {
-                    auto_reconnect : true,
-                    reconnectTries : 17280,
-                    reconnectInterval : 5000
-                }
-            }
-        },
-
-        jwtSecret: "resti!$ret*&key", // JWT secret
     }
 
 };
 
-if ("local" === config.mode) {
-    config.mode = "local";
-}
-else if ("testV101" === config.mode) {
-    config.mode = "testV101";
-}
-else {
-    config.mode = "production";
-}
+config.mode = "testV101";
 
 module.exports = config;
